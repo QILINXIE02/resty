304040d5eae6de83a6ecfb2904d9ea5239e11b45
Qilin Xie <qxie@gicw.org>
2025-08-19 11:29:54 -0700

    changes to vite app

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -4,7 +4,7 @@ import './Footer.scss';
 const Footer = () => {
   return (
     <footer className="Footer">
-      <p>&copy; Qilin Xie @2024 RESTy Inc.</p>
+      <p>&copy; Qilin Xie 2025 RESTy Inc.</p>
     </footer>
   );
 };
